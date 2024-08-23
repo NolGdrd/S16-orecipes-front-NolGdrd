b8c0f10ad6de3e1809e1fc5975dc69b70a80caab
JeanManyama <jean.manyama@gmail.com>
2024-08-23 14:17:13 +0200

    qlqs modif css

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,27 +1,23 @@
-import  "./Card.scss";
+import "./Card.scss";
 
-function Card(){
-  return(
-
-
+function Card() {
+  return (
     <article className="cards">
-
-    <div className="card">
-      <div className="card-header">
-        <img src="img.png" />
-      </div>
-      <div className="card-body">
-        <h2>Titre de la Carte</h2>
-        <p>description.</p>
-        <div className="btn-container">
-          <a href="#" className="btn">
-            Voir Détails
-          </a>
+      <div className="card">
+        <div className="card-header">
+          <img src="img.png" />
+        </div>
+        <div className="card-body">
+          <h2>Titre de la Carte</h2>
+          <p>description.</p>
+          <div className="btn-container">
+            <a href="#" className="btn">
+              Voir Détails
+            </a>
+          </div>
         </div>
       </div>
-    </div>   
-
-  </article>
-  )
+    </article>
+  );
 }
-export default Card
+export default Card;
