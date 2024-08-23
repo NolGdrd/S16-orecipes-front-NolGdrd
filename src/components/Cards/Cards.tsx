ecbf30c1ceef4f4236279fe7a5fdde758c4b9801
JeanManyama <jean.manyama@gmail.com>
2024-08-23 11:38:47 +0200

    Cards

---
+++ src/components/Cards/Cards.tsx
@@ -0,0 +1,23 @@
+function Cards(){
+
+
+  return (<div className="container-list-card">
+  <div className="div-title">
+    <h1>Les recettes oRrecipes</h1>
+  </div>
+  <h2 className="sub-title">Voici nos recettes</h2>
+  <div className="card">
+    <div className="card-header">
+      <img src="img.png"/>
+    </div>
+    <div className="card-body">
+      <h2>Titre de la Carte</h2>
+      <p>description.</p>
+      <div className="btn-container">
+        <a href="#" className="btn">Voir Détails</a>
+      </div>
+    </div>
+  </div>
+</div>)
+}
+export default Cards;
